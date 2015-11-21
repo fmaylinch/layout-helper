12588e2f9b648afcda45880b03ed5fc0aebb9a5a
fmaylinch <ferranmaylinch@gmail.com>
2015-11-21 03:12:11 +0100

    Initial Commit

---
+++ LayoutHelperApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutHelperApp
+//
+//  Created by Ferran Maylinch on 21/11/15.
+//  Copyright © 2015 fmaylinch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
