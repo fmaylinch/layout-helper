6ca911ef8876c729f135623fc00b9d3c3ddb1935
fmaylinch <ferranmaylinch@gmail.com>
2015-11-22 23:20:14 +0100

    Disable NSURLSession cache

--- LayoutHelperApp/AppDelegate.swift
+++ LayoutHelperApp/AppDelegate.swift
@@ -16,9 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        removeUrlCache()
         return true
     }
 
+    // http://stackoverflow.com/questions/30324394/prevent-nsurlsession-from-caching-responses
+    func removeUrlCache() {
+        NSURLCache.setSharedURLCache(NSURLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil))
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
