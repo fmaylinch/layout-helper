be8c46dd4ec7f15e0864594a66cad96dc8b1cb7f
fmaylinch <ferranmaylinch@gmail.com>
2015-11-27 12:45:13 +0100

    Remove code from tests

--- LayoutHelperApp/ViewController.swift
+++ LayoutHelperApp/ViewController.swift
@@ -5,30 +5,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var content: UIView!
-    @IBOutlet weak var content2: UIView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // logAvailableFonts()
-        
-        // will print cell on content view
-        let cell = EventCell(contentView: content, included: true)
-        
-        cell.activity.text = "Yoga (really really hardcore)".uppercaseString
-        cell.gym.text = "Dona 10".uppercaseString
-        cell.startTime.text = "11:00"
-        cell.endTime.text = "12:00"
-        cell.setClubName("CLUB PREMIUM")
-        
-        
-        let cell2 = EventCell(contentView: content2, included: false)
-        
-        cell2.activity.text = "Yoga (really really hardcore)".uppercaseString
-        cell2.gym.text = "Dona 10".uppercaseString
-        cell2.startTime.text = "11:00"
-        cell2.endTime.text = "12:00"
+
+        // Do things here
     }
     
     // http://codewithchris.com/common-mistakes-with-adding-custom-fonts-to-your-ios-app/
